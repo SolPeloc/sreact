282be7d08830c9047af7869e3a56d760382c8c77
Sol Peloc <solpeloc@gmail.com>
2021-12-05 11:51:58 -0300

    desafio2

---
+++ src/componentes/header/Header.js
@@ -0,0 +1,30 @@
+import React, { Fragment } from "react";
+
+const Header = ()=> {
+   return (
+       <>  
+                <h1>Tienda Atia</h1>
+
+                   <nav>
+                        <ul className="estiloLista">
+                            <li><a href="#">Mujer</a></li>
+                            <li><a href="#">Accesorios</a></li> 
+
+                        </ul>
+                        
+                    </nav>        
+
+
+
+       
+
+
+ 
+
+  
+        </>
+
+    )
+}
+
+export default Header;
